7364f4ccccc22ef972ff815f8b303d51a5b7f70b
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-18 12:29:25 -0500

    feat(ui-header): Create component
    Define the base structure of the header component as part of the “project bones” step.

---
+++ src/components/UI/UIHeader.vue
@@ -0,0 +1,19 @@
+<template>
+	<header class="header">header</header>
+</template>
+
+<script>
+export default {
+	name: 'UIHeader',
+	props: {},
+};
+</script>
+
+<style scoped>
+.header {
+	flex: 0 0 auto;
+	padding: 16px;
+	background: #232323;
+	color: #fff;
+}
+</style>
