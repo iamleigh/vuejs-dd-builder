7615351dd08a2e66387d3a049fb876731f652ca2
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-18 13:02:59 -0500

    feat(ui-content): Add drag and drop ability

--- src/components/UI/UIContent.vue
+++ src/components/UI/UIContent.vue
@@ -23,14 +23,52 @@
 
 		<!-- Content Area -->
 		<main class="content-area">
-			<div class="drop-zone">
-				<div class="placeholder">
+			<!-- Drop Zone -->
+			<div
+				class="drop-zone"
+				:class="droppedElements.length ? ' full' : ''"
+				@dragover.prevent="onDragOver"
+				@dragleave="onDragLeave"
+				@drop="onDrop"
+			>
+				<!-- Placeholder -->
+				<div
+					v-if="!droppedElements.length && ghostIndex === null"
+					class="placeholder"
+				>
 					<img
 						src="../../assets/placeholder-image.svg"
 						alt="Image of an element being dragged"
 					/>
 					<p>Drop content blocks from the sidebar here</p>
 				</div>
+
+				<!-- Dropped Elements -->
+				<div
+					v-for="(el, index) in droppedElements"
+					:key="index"
+					class="drop-item"
+					@dragover.prevent="onDragOver(index)"
+					@dragleave="onDragLeave"
+				>
+					<div v-if="el.type === 'TextElement'">
+						<p>The quick brown fox jumps over the lazy dog.</p>
+					</div>
+
+					<div v-else-if="el.type === 'ImageElement'">
+						<img
+							src="https://www.gynprog.com.br/wp-content/uploads/2017/06/wood-blog-placeholder.jpg"
+							alt=""
+						/>
+					</div>
+				</div>
+
+				<!-- Ghost Placeholder -->
+				<div
+					v-if="ghostIndex !== null"
+					:style="ghostStyle"
+					class="ghost-placeholder"
+				></div>
 			</div>
 		</main>
 
@@ -48,15 +86,49 @@ export default {
 	data() {
 		return {
 			isSecondarySidebarVisible: false,
+			isDragging: false,
+			ghostIndex: null,
 			elements: [
 				{ type: 'TextElement', label: 'Text Element' },
 				{ type: 'ImageElement', label: 'Image Element' },
 			],
+			droppedElements: [],
 		};
+	},
+	computed: {
+		ghostStyle() {
+			return {
+				width: '100%',
+				height: '140px',
+				border: '2px dashed #ccc',
+			};
+		},
 	},
 	methods: {
 		toggleSecondarySidebar() {
 			this.isSecondarySidebarVisible = !this.isSecondarySidebarVisible;
+		},
+		onDragStart(element) {
+			this.isDragging = true;
+			event.dataTransfer.setData('element', JSON.stringify(element));
+		},
+		onDragOver(index) {
+			this.ghostIndex =
+				index !== undefined ? index : this.droppedElements.length;
+		},
+		onDragLeave() {
+			this.ghostIndex = null;
+		},
+		onDrop(event) {
+			const element = JSON.parse(event.dataTransfer.getData('element'));
+			if (this.ghostIndex !== null) {
+				this.droppedElements.splice(this.ghostIndex, 0, element);
+			} else {
+				this.droppedElements.push(element);
+			}
+			this.isSecondarySidebarVisible = false;
+			this.isDragging = false;
+			this.ghostIndex = null;
 		},
 	},
 };
@@ -104,5 +176,24 @@ export default {
 .placeholder img {
 	display: block;
 	margin: 0 auto;
+}
+
+.placeholder p {
+	text-align: center;
+}
+
+.drop-zone p {
+	color: #888;
+	font-style: italic;
+}
+
+.drop-item {
+	padding: 16px;
+}
+
+.ghost-placeholder {
+	width: 100%;
+	height: 140px;
+	border: 2px dashed #ccc;
 }
 </style>
