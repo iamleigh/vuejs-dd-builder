9c04fefb6f8a6c57b4e3a3a9c08fdf4bf9c22636
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-18 12:39:47 -0500

    fix(ui-content): Create component
    Define the base structure of the content component as part of the “project bones” step. This includes:
    - Required sidebars
    - Main content area
    - Ability to show/hide the secondary sidebar for elements

---
+++ src/components/UI/UIContent.vue
@@ -0,0 +1,98 @@
+<template>
+	<div class="ui-content">
+		<!-- Left Sidebar -->
+		<aside class="sidebar">
+			<button @click="toggleSecondarySidebar">Elements</button>
+			<button>Export</button>
+		</aside>
+
+		<!-- Secondary Sidebar -->
+		<aside v-if="isSecondarySidebarVisible" class="secondary-sidebar">
+			<h3>Elements</h3>
+			<ul>
+				<li>Text Element</li>
+				<li>Image Element</li>
+			</ul>
+		</aside>
+
+		<!-- Content Area -->
+		<main class="content-area">
+			<div class="drop-zone">
+				<div class="placeholder">
+					<img
+						src="../../assets/placeholder-image.svg"
+						alt="Image of an element being dragged"
+					/>
+					<p>Drop content blocks from the sidebar here</p>
+				</div>
+			</div>
+		</main>
+
+		<!-- Right Sidebar -->
+		<aside class="sidebar">
+			<h3>Settings</h3>
+		</aside>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'UIContent',
+	props: {},
+	data() {
+		return {
+			isSecondarySidebarVisible: false,
+		};
+	},
+	methods: {
+		toggleSecondarySidebar() {
+			this.isSecondarySidebarVisible = !this.isSecondarySidebarVisible;
+		},
+	},
+};
+</script>
+
+<style scoped>
+.ui-content {
+	flex: 1;
+	display: flex;
+	overflow-y: auto;
+}
+
+.content-area {
+	flex: 1;
+	padding: 24px;
+	background: #f5f5f5;
+}
+
+.sidebar {
+	width: 200px;
+	background: #919191;
+	padding: 10px;
+}
+
+.secondary-sidebar {
+	width: 200px;
+	background: #eaeaea;
+	padding: 10px;
+}
+
+.drop-zone {
+	width: 100%;
+	max-width: 620px;
+	margin: 0 auto;
+	background: #fff;
+}
+
+.placeholder {
+	width: 100%;
+	text-align: center;
+	padding: 24px;
+	border: 2px dashed #ccc;
+}
+
+.placeholder img {
+	display: block;
+	margin: 0 auto;
+}
+</style>
