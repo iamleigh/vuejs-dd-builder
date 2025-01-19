cd3ea25052cc9c36b1aa1da2208dae099107406e
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-19 03:03:30 -0500

    feat(ui-button): Create new component
    Create a simple button component.

---
+++ src/components/UI/UIButton.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+	label: {
+		type: String,
+		default: 'Empty Label',
+	},
+});
+
+defineEmits(['click']);
+</script>
+
+<template>
+	<button @click="$emit('click')">
+		{{ label }}
+	</button>
+</template>
