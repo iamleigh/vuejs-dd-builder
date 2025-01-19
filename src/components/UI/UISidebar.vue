026259add792bb5a180d6b98aee6ce5f3e63003f
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-19 03:04:17 -0500

    feat(ui-sidebar): Add floating variation
    Define `float` property to allow a floating version of the component.

--- src/components/UI/UISidebar.vue
+++ src/components/UI/UISidebar.vue
@@ -4,11 +4,15 @@ defineProps({
 		type: String,
 		default: null,
 	},
+	float: {
+		type: Boolean,
+		default: false,
+	},
 });
 </script>
 
 <template>
-	<aside class="sidebar">
+	<aside class="sidebar" :class="{ 'sidebar--float': float }">
 		<h3 v-if="title" class="sidebar__title">{{ title }}</h3>
 		<slot></slot>
 	</aside>
@@ -17,11 +21,17 @@ defineProps({
 <style lang="scss">
 .sidebar {
 	width: 200px;
+	position: relative;
 	background: #919191;
 	padding: 10px;
 
 	&__title {
 		color: #fff;
+	}
+
+	&--float {
+		position: absolute;
+		left: 100%;
 	}
 }
 </style>
