35c85945ef9c92fae2598f0b26b26f3972227718
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-20 09:53:07 -0500

    feat(block-image): Create new component
    This component was created to represent the image element.

---
+++ src/components/UI/Block/BlockImage.vue
@@ -0,0 +1,22 @@
+<template>
+	<BlockWrapper>
+		<img
+			src="https://www.gynprog.com.br/wp-content/uploads/2017/06/wood-blog-placeholder.jpg"
+			alt=""
+			class="block-image"
+		/>
+	</BlockWrapper>
+</template>
+
+<script setup>
+import BlockWrapper from './BlockWrapper.vue';
+</script>
+
+<style lang="scss" scoped>
+.block-image {
+	width: auto;
+	max-height: 120px;
+	display: block;
+	margin: 0 auto;
+}
+</style>
