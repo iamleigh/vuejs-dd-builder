2a5de92e5e10c5afbac9dde974a0c54d30793ba6
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-20 09:54:09 -0500

    feat(block-main): Create new component
    This is the main component for blocks. Depending on the “element” it displays the correct block element content (markup).

---
+++ src/components/UI/Block/BlockMain.vue
@@ -0,0 +1,24 @@
+<template>
+	<BlockText v-if="'TextElement' === element" :value="value" />
+	<BlockImage v-else-if="'ImageElement' === element" :value="value" />
+</template>
+
+<script setup>
+import BlockImage from './BlockImage.vue';
+import BlockText from './BlockText.vue';
+
+defineProps({
+	element: {
+		type: String,
+		required: true,
+		validator: (value) => {
+			// Only allow available elements
+			return ['TextElement', 'ImageElement'].includes(value);
+		},
+	},
+	value: {
+		type: String,
+		required: true,
+	},
+});
+</script>
