942b08686dc14541c4b4e425f4686e7068730def
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-20 09:52:52 -0500

    feat(block-text): Create new component
    This component was created to represent the text element.

---
+++ src/components/UI/Block/BlockText.vue
@@ -0,0 +1,22 @@
+<template>
+	<BlockWrapper>
+		<p>{{ value }}</p>
+	</BlockWrapper>
+</template>
+
+<script setup>
+import BlockWrapper from './BlockWrapper.vue';
+
+defineProps({
+	value: {
+		type: String,
+		required: true,
+	},
+});
+</script>
+
+<style lang="scss" scoped>
+p {
+	color: blue;
+}
+</style>
