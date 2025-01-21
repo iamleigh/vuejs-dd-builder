ddd4a2eb98f6b9740df30b2de7e8b25a2a23ab5f
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-21 17:03:41 -0500

    fix(ui-sidebar): Improve component
    This commit includes a series of improvements for the `UISidebar` component to style it and make it more responsive as well as more flexible with new variants:
    
    - Add support for `vertical` property.
    - Remove old inline styles and import the SCSS stylesheet instead.
    - Include a `vertical` variation to the styles that will be applied for tablets and larger screens only when this class variation is enabled.
    - Remove support in styles for the `float` variation. TBD: Bring back this variation styles in the future IF necessary of remove when cleaning-up.

--- src/components/UI/UISidebar.vue
+++ src/components/UI/UISidebar.vue
@@ -1,4 +1,19 @@
+<template>
+	<aside
+		class="leighton-quito-sidebar"
+		:class="{
+			'sidebar--float': float,
+			'leighton-quito-sidebar--vertical': vertical,
+		}"
+	>
+		<h3 v-if="title" class="sidebar__title">{{ title }}</h3>
+		<slot></slot>
+	</aside>
+</template>
+
 <script setup>
+import { computed } from 'vue';
+
 defineProps({
 	title: {
 		type: String,
@@ -7,64 +22,14 @@ defineProps({
 	float: {
 		type: Boolean,
 		default: false,
+	}, // TBD: Add styles for this variation
+	vertical: {
+		type: Boolean,
+		default: false,
 	},
 });
 </script>
 
-<template>
-	<aside class="sidebar" :class="{ 'sidebar--float': float }">
-		<h3 v-if="title" class="sidebar__title">{{ title }}</h3>
-		<slot></slot>
-	</aside>
-</template>
-
-<style lang="scss">
-.sidebar {
-	width: 200px;
-	position: relative;
-	background: #919191;
-	padding: 10px;
-
-	&__title {
-		color: #fff;
-	}
-
-	&--float {
-		height: 100%;
-		position: absolute;
-		top: 0;
-		left: 100%;
-		margin-left: 1px; // Remove later
-	}
-
-	&__list {
-		margin: 0;
-		padding: 0;
-		list-style: none;
-
-		li {
-			cursor: move;
-			margin: 16px 0;
-			padding: 8px;
-			background: #fff;
-
-			&.sortable-chosen {
-				box-shadow: 0 0 4px black;
-			}
-
-			// STATE: When element is being moved
-			// &.sortable-ghost {
-			// 	background: blue;
-			// }
-
-			&:first-child {
-				margin-top: 0;
-			}
-
-			&:last-child {
-				margin-bottom: 0;
-			}
-		}
-	}
-}
+<style lang="scss" scoped>
+@forward '../../assets/scss/components/ui-sidebar';
 </style>
