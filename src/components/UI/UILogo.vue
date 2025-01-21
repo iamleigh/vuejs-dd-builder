c3650de2666a11d6bccc05263ee6855b4100f0c5
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-21 00:38:34 -0500

    feat(ui-logo): Create new component

---
+++ src/components/UI/UILogo.vue
@@ -0,0 +1,22 @@
+<template>
+	<h1 class="leighton-quito-logo" :class="setClass">
+		<img
+			src="../../assets/img/logo.svg"
+			alt="Logo icon"
+			class="leighton-quito-logo__icon"
+		/>
+		<span class="leighton-quito-logo__text">Leighton Quito</span>
+	</h1>
+</template>
+
+<script setup>
+defineProps({
+	setClass: {
+		type: String,
+	},
+});
+</script>
+
+<style lang="scss">
+@forward '../../assets/scss/components/ui-logo';
+</style>
