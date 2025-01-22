711320cf80a35e0a13ee2e0d379a91bbde740178
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-22 11:46:15 -0500

    fix(builder-toolbox): Improve styles
    - Define a max width for the toolbox.
    - Define block title styles.
    - Define block content basic styles.
    - Depending on the element type define custom styles.

--- src/components/Builder/BuilderToolbox.vue
+++ src/components/Builder/BuilderToolbox.vue
@@ -3,11 +3,11 @@
 		class="leighton-quito-builder-toolbox"
 		:class="{ 'leighton-quito-builder-toolbox--active': active }"
 	>
-		<h3>Elements</h3>
+		<h3 class="leighton-quito-builder-toolbox__title">Elements</h3>
 
 		<draggableComponent
 			tag="ul"
-			class="sidebar__list"
+			class="leighton-quito-builder-toolbox__list"
 			:list="elements"
 			:group="{ name: 'blocks', pull: 'clone', put: false }"
 			:clone="$emit('clone')"
@@ -15,7 +15,29 @@
 			item-key="id"
 		>
 			<template #item="{ element }">
-				<li>{{ element.label }}</li>
+				<li>
+					<span class="title">{{ element.label }}</span>
+					<span class="content">
+						<template v-if="'TextElement' === element.type">
+							The quick brown fox jumps over the lazy dog.
+						</template>
+
+						<template v-else-if="'ImageElement' === element.type">
+							<span
+								:style="{
+									minHeight: '80px',
+									background: 'var(--lq-neutral-200)',
+									padding: '8px',
+									display: 'flex',
+									alignItems: 'center',
+									justifyContent: 'center',
+								}"
+							>
+								<i class="pi pi-image" style="font-size: 2rem"></i>
+							</span>
+						</template>
+					</span>
+				</li>
 			</template>
 		</draggableComponent>
 	</aside>
