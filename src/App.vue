35791485bedccfcef3c31653568bd228f4d6c449
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-22 01:33:19 -0500

    feat: Install `primeicons`
    The icon buttons will require access to icons from the PrimeVue kit and this commit enables it.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import UIHeader from './components/UI/UIHeader.vue';
 import UIContent from './components/UI/UIContent.vue';
+import 'primeicons/primeicons.css';
 </script>
 
 <template>
