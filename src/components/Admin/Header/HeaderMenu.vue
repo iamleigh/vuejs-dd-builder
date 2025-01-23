567665d6de299a62e02e2d1c2a75b8ecedd331a2
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-23 12:42:51 -0500

    fix(builder-header): Clean-up and simplify code
    This commit granulates the code making it mode reusable and maintainable for the future by creating two new components:
    - `HeaderBar`
    - `HeaderMenu`
    
    It also register a new alias `@header` to find elements in the folder faster.

---
+++ src/components/Admin/Header/HeaderMenu.vue
@@ -0,0 +1,41 @@
+<template>
+	<div
+		role="menubar"
+		class="leighton-quito-builder-menubar"
+		:class="{ 'leighton-quito-builder-menubar--active': openSidebar }"
+	>
+		<Button
+			role="menuitem"
+			aria-label="Add Element"
+			icon="pi pi-plus"
+			severity="contrast"
+			@click="$emit('add-element')"
+		/>
+
+		<Button
+			role="menuitem"
+			aria-label="Download"
+			icon="pi pi-download"
+			severity="contrast"
+			@click="$emit('download')"
+		/>
+	</div>
+</template>
+
+<script setup>
+import { Button } from 'primevue';
+
+defineProps({
+	openSidebar: {
+		type: Boolean,
+		default: false,
+		required: true,
+	},
+});
+
+defineEmits(['add-element', 'download']);
+</script>
+
+<style lang="scss" scoped>
+@forward '../../../assets/scss/builder/builder-menubar';
+</style>
