f15c93a68dd98d9f418e39005be5295acf2c1c97
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-26 15:38:46 -0500

    feat(settings-container): Create new component
    This commit splits the element’s “container settings” in its own file for better maintenance in the future and allows user to toggle each option visibility based on the element needs.

---
+++ src/components/Admin/Settings/SettingsContainer.vue
@@ -0,0 +1,47 @@
+<template>
+	<FieldGroup title="Container">
+		<FieldItem v-if="showHeight" title="Height">
+			<InputGroup>
+				<InputNumber v-model="properties.height" :min="0" />
+				<InputGroupAddon>px</InputGroupAddon>
+			</InputGroup>
+		</FieldItem>
+
+		<FieldItem v-if="showPadding" title="Vertical Padding">
+			<InputGroup>
+				<InputNumber v-model="properties.vPadding" />
+				<InputGroupAddon>px</InputGroupAddon>
+			</InputGroup>
+		</FieldItem>
+
+		<FieldItem v-if="showPadding" title="Horizontal Padding">
+			<InputGroup>
+				<InputNumber v-model="properties.hPadding" />
+				<InputGroupAddon>px</InputGroupAddon>
+			</InputGroup>
+		</FieldItem>
+	</FieldGroup>
+</template>
+
+<script setup>
+import { InputGroup, InputGroupAddon, InputNumber } from 'primevue';
+import FieldGroup from '../../Field/FieldGroup.vue';
+import FieldItem from '../../Field/FieldItem.vue';
+
+defineProps({
+	properties: {
+		type: Object,
+		default: () => ({}),
+	},
+	showHeight: {
+		type: Boolean,
+		default: false,
+	},
+	showPadding: {
+		type: Boolean,
+		default: false,
+	},
+});
+</script>
+
+<style lang="scss" scoped></style>
