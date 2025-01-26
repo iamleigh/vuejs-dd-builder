8c8afd3317265fcc073a00cba4c4cbe1da13f7fa
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-26 15:43:54 -0500

    feat(settings-image): Create new component
    This commit splits the element’s “image settings” to its own file for better maintenance in the future.

---
+++ src/components/Admin/Settings/SettingsImage.vue
@@ -0,0 +1,50 @@
+<template>
+	<FieldGroup title="Default Image">
+		<UIRadioImageGroup
+			:group="id"
+			:options="defaultOptions"
+			:default-option="value"
+		/>
+	</FieldGroup>
+</template>
+
+<script setup>
+import FieldGroup from '../../Field/FieldGroup.vue';
+import UIRadioImageGroup from '../../UI/UIRadioImageGroup.vue';
+
+defineProps({
+	id: {
+		type: Number,
+		default: 0,
+	},
+	default: {
+		type: String,
+		default: '',
+	},
+	properties: {
+		type: Object,
+		default: () => ({}),
+	},
+});
+
+const defaultOptions = ref([
+	{
+		label: 'Food',
+		value: '/assets/banner-food.webp',
+	},
+	{
+		label: 'Tourism',
+		value: '/assets/banner-tourism.webp',
+	},
+	{
+		label: 'Medicine',
+		value: '/assets/banner-medicine.webp',
+	},
+	{
+		label: 'Architecture',
+		value: '/assets/banner-architecture.webp',
+	},
+]);
+</script>
+
+<style lang="scss" scoped></style>
